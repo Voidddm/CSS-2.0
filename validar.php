622f5195885e7d08be8e4e899623fafc09345a50
Voidddm <105731231+Voidddm@users.noreply.github.com>
2024-06-12 00:16:38 -0400

    Create validar.php

---
+++ validar.php
@@ -0,0 +1,40 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // Datos de conexión a la base de datos
+    $servername = "db-2024.mysql.database.azure.com";
+    $db_username = "jim";
+    $db_password = "2839064Void";
+    $dbname = "db-ticket";
+
+    // Crear conexión
+    $conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+    // Verificar conexión
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Recibir los datos del formulario
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+
+    // Consulta SQL para verificar el usuario y la contraseña
+    $sql = "SELECT * FROM clientes WHERE cliente_id = '$username' AND pass = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Si hay al menos un resultado, el usuario y la contraseña son correctos
+        // Redirigir a home.html
+        header("Location: home.html");
+        exit(); // Asegura que el script se detenga aquí y no siga ejecutándose
+    } else {
+        // Si no hay resultados, el usuario y/o la contraseña son incorrectos
+        echo "Usuario y/o contraseña incorrectos";
+    }
+
+    $conn->close();
+} else {
+    echo "Acceso denegado";
+}
+?>
